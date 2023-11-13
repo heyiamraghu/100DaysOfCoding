5b3b76817e355cd13da69dd5f67ceb81ff5ee0fd
heyiamraghu <hey.iamraghu@gmail.com>
2023-11-13 19:41:54 +0530

    Learned Structs, properties, methods, computed properties, and access control

--- day 10/MyPlayground.playground/Contents.swift
+++ day 10/MyPlayground.playground/Contents.swift
@@ -50,13 +50,69 @@ struct Manager {
     var vacationAllocated: Int = 14
     var vacationTaken = 0
     var vacationRemaining: Int {
-        vacationAllocated - vacationTaken
+        get {
+            vacationAllocated - vacationTaken
+        }
+        set {
+            vacationAllocated = vacationTaken + newValue
+        }
     }
 }
 
 var subha = Manager(name: "Subha")
 subha.vacationAllocated
 subha.vacationTaken += 4
-subha.vacationRemaining
-subha.vacationAllocated
-subha.vacationTaken
+subha.vacationRemaining = 5
+print(subha.vacationRemaining)
+
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now - \(score). The old value was - \(oldValue)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 4
+game.score += 5
+
+struct App {
+    var contacts: [String] = [] {
+        willSet {
+            print("Current list of contacts - \(contacts)")
+            print("You're adding - \(newValue) to the list.")
+        }
+    }
+    
+}
+
+var myContacts = App()
+myContacts.contacts.append("Raghu")
+myContacts.contacts.append("Raja")
+
+struct Player {
+    let name: String
+    let number: Int
+    
+    init(name: String) {
+        self.name = name
+        number = Int.random(in: 1...99)
+    }
+}
+
+let player = Player(name: "Raghu")
+print(player)
+
+class Opener {
+    let name: String
+    let number: Int
+    init(name: String, number: Int) {
+        self.name = name
+        self.number = number
+    }
+}
+
+let opener = Opener(name: "Raghu", number: 17)
+print(opener)
