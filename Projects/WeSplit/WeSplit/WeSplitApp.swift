d0e2ac87b9bcf3b38c4a9b7e3b857d1b407100e8
heyiamraghu <hey.iamraghu@gmail.com>
2023-11-14 19:24:28 +0530

    Completed the WeSplit app

---
+++ Projects/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Sampath, Raghupathy on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
