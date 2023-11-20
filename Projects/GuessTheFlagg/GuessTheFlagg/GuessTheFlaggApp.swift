b5f85182a5006e10d49675af9532588b2c35fd64
heyiamraghu <hey.iamraghu@gmail.com>
2023-11-21 04:02:01 +0530

    Completed GuessTheFlag project

---
+++ Projects/GuessTheFlagg/GuessTheFlagg/GuessTheFlaggApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlaggApp.swift
+//  GuessTheFlagg
+//
+//  Created by Sampath, Raghupathy on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlaggApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
