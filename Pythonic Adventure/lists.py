ce5610a6a6534bd349aedf004e02c5588bbca9d1
heyiamraghu <hey.iamraghu@gmail.com>
2024-01-19 19:53:12 +0530

    Added more input methods to mimic the ecommerce store

--- Pythonic Adventure/lists.py
+++ Pythonic Adventure/lists.py
@@ -1,5 +1,7 @@
 products = ["Bat", "Batting Gloves", "Batting Pads", "Keeping Gloves", "Keeping Pads"]
 bat_brands = ["Adidas", "GM", "Gray Nicholls", "SS", "SG", "Kookaburra", "MRF"]
+handle_shapes = ["Round", "Oval", "Semi-Oval"]
+toe_shapes = ["Square", "SemiConcave", "Round"]
 
 print("Welcome to Raghu's Cricket Shop")
 
@@ -15,3 +17,24 @@ if product == "Bat":
     print(f"Great! We've bats from the following brands,")
     for bat_brand in bat_brands:
         print(f"- {bat_brand}")
+bat_selected = input("Type your bat's name here: ")
+
+print("**************************************************")
+
+customs_required = input("Do you need any customizations? (yes/no) ")
+if customs_required == "yes":
+    print("Please choose your toe shape from the below list,")
+    for t in toe_shapes:
+        print(f"- {t}")
+toe_shape = input("Type your toe shape here from the above list: ")
+
+print("******************************************************")
+
+print("Please choose your handle shape from the below list,")
+for h in handle_shapes:
+    print(f"- {h}")
+handle_shape = input("Type your handle shape here: ")
+
+print(f"Thank you for your order!")
+print(f"{customer_name}, here's your order details,")
+print(f"You've ordered a {bat_selected} {product} with the following customisations - {handle_shape} handle shape and {toe_shape} toe shape.")
