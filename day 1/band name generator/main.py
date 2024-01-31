69585e97686ba08527c7f0a3f1ea1465ee83015f
heyiamraghu <hey.iamraghu@gmail.com>
2024-01-31 18:46:43 +0530

    Day 1 is complete

---
+++ day 1/band name generator/main.py
@@ -0,0 +1,8 @@
+# Band name generator
+
+print("Welcome to the Band Name Generator.")
+city_name = input("What's the name of the city you grew up in?\n")
+pets_name = input("What's the name of your pet?\n")
+
+band_name = city_name + " " + pets_name
+print(f"Your band could be named - {band_name}")
---
+++ day 1/printing.py
@@ -0,0 +1,12 @@
+name = "Raghu"
+print(name)
+print("Day 1 - Python Print Function")
+
+print("Hello World!\nHello World!\nHello World!")
+
+print("hello, " + name)
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
