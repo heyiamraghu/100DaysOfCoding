2ffe1dc517fb6f7882a982fc05bfcdb93700ed1c
heyiamraghu <hey.iamraghu@gmail.com>
2024-02-23 11:11:27 +0530

    Diving into the systems programming - understanding how computer systems work

---
+++ C/c.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello World\n");
+    printf("sqrt(4) is %f\n", sqrt(4));
+    return EXIT_SUCCESS;
+}
---
+++ C/vars.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    int i, j, k;
+    char letter;
+    float winpct;
+    double pi;
+
+    x = 7;
+    k = x + 2;
+    letter = 'A';
+    letter = letter + 1;
+    pi = 3.1415926;
+    winpct = 11 / 2.0;
+    j = 11 / 2;
+    x = k % 2;
+
+    //     printf(x);
+    //     printf(i);
+    printf("Calculations!\n");
+    return 0;
+}
