09c739e243922d3eaa36a6890ced1374e7815e81
heyiamraghu <hey.iamraghu@gmail.com>
2024-03-03 21:51:08 +0530

    Dipping my toes into Zed's C book

---
+++ C/zed/exercises/ex1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*This is the syntax for multi-line comment*/
+int main(int argc, char const *argv[])
+{
+    int distance = 200;
+    printf("You are %d miles away.\n", distance);
+    return 0;
+}
---
+++ C/zed/exercises/ex3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int age = 66;
+    int height = 72;
+
+    printf("I am %d years old.\n", age);
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}
